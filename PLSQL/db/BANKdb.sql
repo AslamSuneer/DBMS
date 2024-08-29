aa4691bd89ed680a1fe0fa1a126043db92b478e2
AslamSuneer <103572084+AslamSuneer@users.noreply.github.com>
2024-08-29 20:14:31 +0530

    Add files via upload

---
+++ PLSQL/db/BANKdb.sql
@@ -0,0 +1,9 @@
+ create table acc(acno number primary key,BAL number);
+
+Table created.
+
+SQL> desc acc;
+ Name                                      Null?    Type
+ ----------------------------------------- -------- ---------------------------
+ ACNO                                      NOT NULL NUMBER
+ BAL                                                NUMBER
