8d378b87a278c51b1b71a805816f5d137b558154
AslamSuneer <103572084+AslamSuneer@users.noreply.github.com>
2024-08-29 20:19:11 +0530

    Create BankCreditDebitDB.sql

---
+++ PLSQL/db/BankCreditDebitDB.sql
@@ -0,0 +1,5 @@
+CREATE TABLE accounts (acc_num NUMBER(5) NOT NULL PRIMARY KEY,bal NUMBER(10));
+
+INSERT INTO accounts values(11,10000);
+INSERT INTO accounts values(24,50000);
+INSERT INTO accounts values(45,2000);
