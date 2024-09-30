a91794f39bdf17d8a35d37dfa93c80bf424fec98
Aslam Suneer <103572084+AslamSuneer@users.noreply.github.com>
2024-09-30 23:49:34 +0530

    Update StudentTriggerTotalAvg.sql

--- PLSQL/StudentTriggerTotalAvg.sql
+++ PLSQL/StudentTriggerTotalAvg.sql
@@ -2,10 +2,15 @@
 CREATE TABLE Student (Sid INT PRIMARY KEY,name VARCHAR(50),Subj1 INT,Subj2 INT,Subj3 INT,Total INT DEFAULT 0,Avg DECIMAL(5, 2) DEFAULT 0.00
 );
 
-CREATE TRIGGER calculate_total_avg
+CREATE OR REPLACE TRIGGER calculate_total_avg
 BEFORE INSERT ON Student
 FOR EACH ROW
 BEGIN
+    -- Calculate the total of the marks for Subj1, Subj2, and Subj3
+    --:NEW.Total := :NEW.Subj1 + :NEW.Subj2 + :NEW.Subj3;
+    
+    -- Calculate the average of the marks
+   -- :NEW.Avg := :NEW.Total / 3;
     SET NEW.Total = NEW.Subj1 + NEW.Subj2 + NEW.Subj3;
     SET NEW.Avg = (NEW.Subj1 + NEW.Subj2 + NEW.Subj3) / 3;
 END;
