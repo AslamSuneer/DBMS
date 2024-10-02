f0fb5e6b75c95a8077ba5192b0c37db96c68528e
Aslam Suneer <103572084+AslamSuneer@users.noreply.github.com>
2024-10-02 14:31:31 +0530

    Update traindb.sql

--- PLSQL/db/traindb.sql
+++ PLSQL/db/traindb.sql
@@ -1,16 +1,14 @@
-CREATE TABLE Train (
-    TNO INT AUTO_INCREMENT PRIMARY KEY,
-    Tname VARCHAR(20),
-    TDate DATE,
-    Destination VARCHAR(20),
-    Seats INT
-);
+TABLE CREATION:
+CREATE TABLE Train (TNO INT AUTO_INCREMENT PRIMARY KEY,Tname VARCHAR(20),TDate DATE,Destination VARCHAR(20),Seats INT);
 
-INSERT INTO Train(Tname,TDate,Destination,Seats)VALUES('express1','16-apr-2024','mumbai',300);
-INSERT INTO Train(Tname,TDate,Destination,Seats)VALUES('express2','16-apr-2024','chennai',250);
-INSERT INTO Train(Tname,TDate,Destination,Seats)VALUES('express3','17-apr-2024','delhi',100);
-INSERT INTO Train(Tname,TDate,Destination,Seats)VALUES('express4','19-apr-2024','kolkata',100);
+INSERTION:
+INSERT INTO Train(Tname, TDate, Destination, Seats)VALUES ('express1', '2024-04-16', 'mumbai', 300);
 
+INSERT INTO Train(Tname, TDate, Destination, Seats)VALUES ('express2', '2024-04-16', 'chennai', 250);
+
+INSERT INTO Train(Tname, TDate, Destination, Seats)VALUES ('express3', '2024-04-17', 'delhi', 100);
+
+INSERT INTO Train(Tname, TDate, Destination, Seats)VALUES ('express4', '2024-04-19', 'kolkata', 100);
 
 select*from Train;
 
