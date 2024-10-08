dc199a75ed95342dd7b37580c457af6d7e126efb
Aslam Suneer <103572084+AslamSuneer@users.noreply.github.com>
2024-10-08 19:02:34 +0530

    Update traindb.sql

--- PLSQL/db/traindb.sql
+++ PLSQL/db/traindb.sql
@@ -1,21 +1,68 @@
 TABLE CREATION:
-CREATE TABLE Train (TNO INT AUTO_INCREMENT PRIMARY KEY,Tname VARCHAR(20),TDate DATE,Destination VARCHAR(20),Seats INT);
+Create table Train(TNO NUMBER PRIMARY KEY,Tname VARCHAR2(20),TDate DATE,Destination VARCHAR2(20),Seats NUMBER);
+
+Table created.
+
+SQL> CREATE SEQUENCE TrainSeq START WITH 1 INCREMENT BY 1;
+
+Sequence created.
+
+SQL> desc Train;
+ Name										     Null?    Type
+ ----------------------------------------------------------------------------------- -------- --------------------------------------------------------
+ TNO										     NOT NULL NUMBER
+ TNAME										      VARCHAR2(20)
+ TDATE										      DATE
+ DESTINATION									      VARCHAR2(20)
+ SEATS										      NUMBER
+
 
 INSERTION:
-INSERT INTO Train(Tname, TDate, Destination, Seats)VALUES ('express1', '2024-04-16', 'mumbai', 300);
+INSERT INTO Train(TNO,Tname, TDate, Destination, Seats)VALUES (TrainSeq.NEXTVAL,'express1',TO_DATE('15-OCT-2024','DD-MON-YYYY'), 'mumbai', 300);
 
-INSERT INTO Train(Tname, TDate, Destination, Seats)VALUES ('express2', '2024-04-16', 'chennai', 250);
+INSERT INTO Train(TNO,Tname, TDate, Destination, Seats)VALUES (TrainSeq.NEXTVAL,'express2',TO_DATE('16-OCT-2024','DD-MON-YYYY'), 'chennai', 250);
 
-INSERT INTO Train(Tname, TDate, Destination, Seats)VALUES ('express3', '2024-04-17', 'delhi', 100);
+INSERT INTO Train(TNO,Tname, TDate, Destination, Seats)VALUES (TrainSeq.NEXTVAL,'express3', TO_DATE('17-OCT-2024','DD-MON-YYYY'),'delhi', 100);
 
-INSERT INTO Train(Tname, TDate, Destination, Seats)VALUES ('express4', '2024-04-19', 'kolkata', 100);
+INSERT INTO Train(TNO,Tname, TDate, Destination, Seats)VALUES (TrainSeq.NEXTVAL,'express4', TO_DATE('19-OCT-2024','DD-MON-YYYY'),'kolkata', 100);
 
 select*from Train;
+
+       TNO TNAME		TDATE		   DESTINATION		     SEATS
+---------- -------------------- ------------------ -------------------- ----------
+	 1 express1		15-OCT-24	   mumbai		       300
+	 2 express2		16-OCT-24	   chennai		       250
+	 3 express3		17-OCT-24	   delhi		       100
+	 4 express4		19-OCT-24	   kolkata		       100
+
 
 SQL>COMMIT;
 
-INSERT INTO Train(Tname,TDate,Destination,Seats)VALUES('express5','20-apr-2024','goa',300);
-select*from Train;
+INSERT INTO Train(TNO,Tname,TDate,Destination,Seats)VALUES(TrainSeq.NEXTVAL,'express5', TO_DATE('20-OCT-2024','DD-MON-YYYY'),'goa',300);
 
-SQL>ROLLBACK;
-select*from Train;
+SQL> select*from Train;
+
+       TNO TNAME		TDATE		   DESTINATION		     SEATS
+---------- -------------------- ------------------ -------------------- ----------
+	 1 express1		15-OCT-24	   mumbai		       300
+	 2 express2		16-OCT-24	   chennai		       250
+	 3 express3		17-OCT-24	   delhi		       100
+	 4 express4		19-OCT-24	   kolkata		       100
+	 5 express5		20-OCT-24	   goa			       300
+
+
+
+SQL> ROLLBACK;
+
+Rollback complete.
+
+
+SQL> select*from Train;
+
+       TNO TNAME		TDATE		   DESTINATION		     SEATS
+---------- -------------------- ------------------ -------------------- ----------
+	 1 express1		15-OCT-24	   mumbai		       300
+	 2 express2		16-OCT-24	   chennai		       250
+	 3 express3		17-OCT-24	   delhi		       100
+	 4 express4		19-OCT-24	   kolkata		       100
+
